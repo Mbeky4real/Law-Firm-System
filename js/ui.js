5e8878d848e87e2e4cd5f9de878b0f61f4b41422
Mbeky4real <mbeky4real@gmail.com>
2026-05-15 15:39:25 +0300

    Add esc helper to ui module

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,10 @@
 // MOLMS UI helper module
+function esc(x){
+  return (x ?? '').toString().replace(/[&<>\"']/g, m => ({
+    '&':'&amp;',
+    '<':'&lt;',
+    '>':'&gt;',
+    '\"':'&quot;',
+    \"'\":'&#039;'
+  }[m]))
+}
