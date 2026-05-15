1588ee4a9ecefa5c8094ad33fed5272679fd63fa
Mbeky4real <mbeky4real@gmail.com>
2026-05-15 15:55:31 +0300

    Add DOM helper functions to ui module

--- js/ui.js
+++ js/ui.js
@@ -8,3 +8,14 @@ function esc(x){
     \"'\":'&#039;'
   }[m]))
 }
+function $(id){
+  return document.getElementById(id)
+}
+
+function val(id){
+  return $(id).value.trim()
+}
+
+function clear(id){
+  $(id).value = ''
+}
