030ed5f990a2153e63b71cc2f0c618b7e830a4d2
Mbeky4real <mbeky4real@gmail.com>
2026-05-17 22:26:53 +0300

    Add read helper to storage module

--- js/storage.js
+++ js/storage.js
@@ -2,3 +2,10 @@
 function write(k,v){
   localStorage.setItem(k,JSON.stringify(v))
 }
+function read(k){
+  try{
+    return JSON.parse(localStorage.getItem(k)||'[]')
+  }catch(e){
+    return []
+  }
+}
